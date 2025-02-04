e2eac8cf05fdcf3222ced1fa2c42d09f54bcde15
amir-ammar <am7099533@gmail.com>
2025-02-04 09:06:31 +0200

    init commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/demo")
+@AllArgsConstructor
+@EnableCaching
+public class DemoController {
+
+    @GetMapping("/dummy")
+    public ResponseEntity<String> getDummy() {
+        System.out.println("This is a dummy endpoint");
+        return ResponseEntity.ok("This is a dummy endpoint");
+    }
+}
