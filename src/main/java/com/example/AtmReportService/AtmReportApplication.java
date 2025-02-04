77b03a97266ca25a4b729021922c0c0017ba9869
amir-ammar <am7099533@gmail.com>
2025-02-04 09:11:16 +0200

    fix: docker file

--- src/main/java/com/example/AtmReportService/AtmReportApplication.java
+++ src/main/java/com/example/AtmReportService/AtmReportApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.AtmReportService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AtmReportApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AtmReportApplication.class, args);
 	}
 
 }
--- src/main/java/com/example/AtmReportService/controller/DemoController.java
+++ src/main/java/com/example/AtmReportService/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.AtmReportService.controller;
 
 import java.util.List;
 import java.util.Optional;
